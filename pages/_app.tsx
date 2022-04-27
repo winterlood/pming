0c311acf7949671c9bd75a77ecf42ee896a94c0f
이정환 <king199777@gmail.com>
2022-04-27 11:07:37 +0900

    GA 스트림 속성 변경

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,22 +2,25 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
 
+const gaScript = `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-XEV7PJC28X');
+`;
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <script
           async
-          src="https://www.googletagmanager.com/gtag/js?id=G-FTGWXZFL8L"
+          src="https://www.googletagmanager.com/gtag/js?id=G-XEV7PJC28X"
         ></script>
+
         <script
           dangerouslySetInnerHTML={{
-            __html: `
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', 'G-FTGWXZFL8L');
-          `,
+            __html: gaScript,
           }}
         />
       </Head>
