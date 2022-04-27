cc872c00f3fbd9dac96ea1acce873de1c872f966
이정환 <king199777@gmail.com>
2022-04-27 11:34:16 +0900

    update favicon with ga tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,26 +1,27 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import Head from "next/head";
-
-const gaScript = `
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-XEV7PJC28X');
-`;
+import favicon from "public/favicon.ico";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
+        <title>프밍</title>
+        <link rel="shortcut icon" href={favicon.src} type="image/x-icon" />
+        {/*  eslint-disable-next-line @next/next/next-script-for-ga */}
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-XEV7PJC28X"
         ></script>
-
         <script
           dangerouslySetInnerHTML={{
-            __html: gaScript,
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+     function gtag(){dataLayer.push(arguments);}
+     gtag('js', new Date());
+     
+     gtag('config', 'G-XEV7PJC28X');`,
           }}
         />
       </Head>
